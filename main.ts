633902e386435db0b3e4d972898c0282ba06d7a4
jruimot0704 <122780882+jruimot0704@users.noreply.github.com>
2023-05-22 10:29:10 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,24 @@
-input.onGesture(Gesture.ScreenDown, function () {
-    radio.sendString("parar")
-})
-input.onButtonPressed(Button.A, function () {
-    basic.showArrow(ArrowNames.West)
-    radio.sendString("izquierda")
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showArrow(ArrowNames.North)
-    radio.sendString("alante")
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showArrow(ArrowNames.East)
-    radio.sendString("derecha")
-})
-radio.setGroup(141221)
+radio.setGroup(208)
 basic.forever(function () {
-	
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+    basic.pause(100)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+    basic.pause(100)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+    basic.pause(100)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendString("izquierda")
+        basic.showArrow(ArrowNames.West)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        radio.sendString("derecha")
+        basic.showArrow(ArrowNames.East)
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        radio.sendString("adelante")
+        basic.showArrow(ArrowNames.North)
+    }
 })
